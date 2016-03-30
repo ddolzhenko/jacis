0b7ac7ab9dcaa1ecb37181cb75bbd0ea27ef4891
Dmitry Dolzhenko <dddolzhenko@luxoft.com>
2016-03-30 15:40:21 +0300

    main added

---
+++ jacis/__main__.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2016 Dmitry Dolzhenko
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#-------------------------------------------------------------------------------
+
+"""Main entry point to jacis command line
+"""
+
+#-------------------------------------------------------------------------------
+
+__author__ = "Dmitry Dolzhenko"
+__email__  = "d.dolzhenko@gmail.com"
+
+#-------------------------------------------------------------------------------
+
+import argparse
+
+#-------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser()
+   
+    parser.add_argument("update", help="Copies or updates folder from external source")
+
+    args = parser.parse_args()
+
+    print(args.update)
+  
+
+if __name__ == "__main__":
+    import sys
+    print("call: '{}'".format(" ".join(sys.argv)))
+    main()
+    print("\n>end<\n")
